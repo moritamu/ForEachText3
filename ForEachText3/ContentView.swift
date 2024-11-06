b72cff0c050c12d4dfbcee93ec5083cc3d69b0b2
morisum <moris@r.sannet.ne.jp>
2024-11-07 06:57:18 +0900

    ListとForEachを完成。コード補完で簡単にできる。.onで.onDeleteが出て、さらにfunc Deleteまで書いてくれる。

--- ForEachText3/ContentView.swift
+++ ForEachText3/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+struct Fluits: Identifiable {
+    let id = UUID()
+    let name: String
+}
+
 struct ContentView: View {
+    @State var fluits = [
+        Fluits(name: "りんご")
+        , Fluits(name: "みかん")
+        , Fluits(name: "ぶどう")
+        , Fluits(name: "いちご")
+        , Fluits(name: "あばか")
+        , Fluits(name: "なつめ")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(fluits) { fluit in
+                Text(fluit.name)
+            }
+            .onDelete(perform: delete)
         }
-        .padding()
+    }
+    func delete(offsets: IndexSet) {
+        fluits.remove(atOffsets: offsets)
     }
 }
 
